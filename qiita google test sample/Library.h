3d13591b287017a10ab6d08dd7ca12e66d76f8d5
drumath <2237k.drumath@gmail.com>
2019-05-05 23:39:27 +0900

    負の値に対する処理

--- qiita google test sample/Library.h
+++ qiita google test sample/Library.h
@@ -1,12 +1,14 @@
 #pragma once
 
 int euclid(int n, int m) {
+	if (n < 0 || m < 0)
+		return -1;
 	if (n < m) {
 		int tmp = n;
 		n = m;
 		m = tmp;
 	}
 	if (m == 0)
-		return m;
+		return n;
 	return euclid(m, n % m);
 }
